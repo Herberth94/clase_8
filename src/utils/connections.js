5a589082bc5be550a5c38925a36ea55fe8b55cec
Herberth Orlando Mancía Cisneros <her_m.c@hotmail.com>
2023-06-10 23:35:26 -0600

    se agrego a la base de datos en especifico

--- src/utils/connections.js
+++ src/utils/connections.js
@@ -2,7 +2,12 @@ import { connect } from "mongoose";
 export async function connectMongo() {
   try {
     await connect(
-      "mongodb+srv://admin:F3wBDRmov2yob7pt@ecommerce.eq2fgne.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://admin:F3wBDRmov2yob7pt@ecommerce.eq2fgne.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "ecommerce" // Especifica el nombre de tu base de datos aquí
+      }
     );
     console.log("plug to mongo!");
   } catch (e) {
